5cc8d1ea060326fa9b6b46e37e21c5a1ed76e5ee
leander006 <leanderdsilva06@gmail.com>
2024-03-01 23:29:43 +0530

    user sign up and sign in

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono';
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { sign } from 'hono/jwt'
+import { sign, verify } from 'hono/jwt'
 
 // Create the main Hono app
 type Bindings = {
@@ -9,6 +9,22 @@ type Bindings = {
 	JWT_SECRET:string,
     }
 const app = new Hono<{ Bindings: Bindings }>();
+
+app.use('/api/v1/blog/*', async (c, next) => {
+	const jwt = c.req.header('Authorization');
+	if(!jwt){
+		c.status(401)
+		return c.json({error:"No token found"})
+	}
+	const token = jwt.split(' ')[1];
+	const payload = await verify(token,c.env.JWT_SECRET)
+	if(!payload){
+		c.status(404)
+		return c.json({error:"Unauthorize"})
+	}
+	c.set("jwtPayload",payload.id)
+	await next()
+})
 
 app.post('/api/v1/signup', async(c) => {
 	const prisma = new PrismaClient({
@@ -28,7 +44,6 @@ app.post('/api/v1/signup', async(c) => {
 			c.status(404);
 			return c.json({error:"User exist"})
 		}
-
 		await prisma.user.create({
 			data:{
 				username:body.username,
@@ -74,18 +89,64 @@ app.post('/api/v1/signin', async(c) => {
 	  }
 })
 
-app.get('/api/v1/blog/:id', (c) => {
+app.get('/api/v1/blog/:id', async(c) => {
+	 
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env.DATABASE_URL,
+	  }).$extends(withAccelerate())
+
 	const id = c.req.param('id')
 	console.log(id);
-	return c.text('get blog route')
+	try {
+		const blog = await prisma.post.findUnique({
+			select:{
+				title:true,
+				content:true,
+				published:true,
+				author:true
+			},
+			where:{
+				id
+			},
+			
+		})
+		console.log(blog);
+		c.status(200)
+		return c.json(blog)
+		
+	} catch (error) {
+		c.status(404)
+		return c.json(error)
+	}
+
 })
 
-app.post('/api/v1/blog', (c) => {
+app.post('/api/v1/blog', async(c) => {
 
-	return c.text('signin route')
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env.DATABASE_URL,
+	  }).$extends(withAccelerate())
+	  
+	try {
+		const body = await c.req.json()
+		const blog = await prisma.post.create({
+			data:{
+				title:body.title,
+				content:body.content,
+				authorId:c.get("jwtPayload"),
+			}
+		})
+		console.log(blog);
+		c.status(200)
+		return c.json({message:"Blog created successfully"})
+	} catch (error) {
+		c.status(404)
+		return c.json({error})
+	}
 })
 
 app.put('/api/v1/blog', (c) => {
+	
 	return c.text('signin route')
 })
 
